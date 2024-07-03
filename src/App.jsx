fff7fac9bdebbe02d24c067e8a87b2efa7d9f4f6
Ashkan <ashkan0.m125@gmail.com>
2024-07-03 11:45:33 +0330

    working on main page, content part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./app.css";
-import Header from "./Components/Header";
+
+import MainPage from "./pages/MainPage";
 
 function App() {
   const [isLight, setIsLight] = useState(true);
   return (
     <div className="App" data-theme={isLight ? "light" : "dark"}>
-      <Header toggleTheme={() => setIsLight(!isLight)} background={isLight} />
+      <MainPage toggleTheme={() => setIsLight(!isLight)} background={isLight} />
     </div>
   );
 }
