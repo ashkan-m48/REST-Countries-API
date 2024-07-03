703c05c69451dd1e4e474d6a87095d40c7562ace
Ashkan <ashkan0.m125@gmail.com>
2024-07-03 11:37:14 +0330

    finish navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import "./app.css";
+import Header from "./Components/Header";
+
 function App() {
-  return <></>;
+  const [isLight, setIsLight] = useState(true);
+  return (
+    <div className="App" data-theme={isLight ? "light" : "dark"}>
+      <Header toggleTheme={() => setIsLight(!isLight)} background={isLight} />
+    </div>
+  );
 }
 
 export default App;
